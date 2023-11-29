b18e51dea19c1028ab0b83bcc1e5375282d482dc
FujiSupanatz <96250376+FujiSupanatz@users.noreply.github.com>
2023-11-29 20:02:24 +0700

    fix count phone bra bbra

--- ihatearduino/ihatearduino.ino
+++ ihatearduino/ihatearduino.ino
@@ -1,79 +1,105 @@
-#include <Wire.h>
-#include <EEPROM.h>
-#include <LiquidCrystal_I2C.h>
-#include <Keypad.h>
+  #include <Wire.h>
+  #include <EEPROM.h>
+  #include <LiquidCrystal_I2C.h>
+  #include <Keypad.h>
 
-String phoneNumber = "";
+  String phoneNumber = "";
+  bool continueCheckSensors = false;
+  unsigned long previousMillis = 0;
+  const long interval = 400;  // Interval between sensor readings in milliseconds
 
-const int trigPin = 3; // Trig pin of the ultrasonic sensor
-const int echoPin = 2; // Echo pin of the ultrasonic sensor
-int bottleCount = 1; // Variable to count bottles
-const byte ROWS = 4;
-const byte COLS = 4;
+  const int trigPin = 3;
+  const int echoPin = 2;
+  int bottleCount = 0;
+  const byte ROWS = 4;
+  const byte COLS = 4;
 
-void saveCountToEEPROM(int count, String number);
-void readCountFromEEPROM();
-void checkSensors();
+  void saveCountToEEPROM(int count, String number);
+  void readCountFromEEPROM();
+  void checkSensors();
+  void displayInfoOnLCD();
+  void checkPoints(); // Added function
 
-char customKeymap[ROWS][COLS] = {
-  {'1', '2', '3', 'A'},
-  {'4', '5', '6', 'B'},
-  {'7', '8', '9', 'C'},
-  {'*', '0', '#', 'D'}
-};
+  char customKeymap[ROWS][COLS] = {
+    {'1', '2', '3', 'A'},
+    {'4', '5', '6', 'B'},
+    {'7', '8', '9', 'C'},
+    {'*', '0', '#', 'D'}
+  };
 
-LiquidCrystal_I2C lcd(0x27, 16, 2);
-byte rowPins[ROWS] = {11, 10, 9, 8}; // Replace with actual row pin numbers (connected to pins 4 to 11)
-byte colPins[COLS] = {7, 6, 5, 4};   // Replace with actual column pin numbers
-Keypad keypad = Keypad(makeKeymap(customKeymap), rowPins, colPins, ROWS, COLS);
+  LiquidCrystal_I2C lcd(0x27, 16, 2);
+  byte rowPins[ROWS] = {11, 10, 9, 8};
+  byte colPins[COLS] = {7, 6, 5, 4};
+  Keypad keypad = Keypad(makeKeymap(customKeymap), rowPins, colPins, ROWS, COLS);
 
-bool isBKeyPressed = false; // Flag to indicate if 'B' key is pressed
+  #define MAX_PHONES 100
 
-void setup() {
-  Serial.begin(9600); // Initialize serial communication for debugging
-  lcd.init(); // Initialize the LCD
-  lcd.backlight(); // Turn on the backlight of the LCD
-  pinMode(trigPin, OUTPUT); // Set trigPin as an output
-  pinMode(echoPin, INPUT); // Set echoPin as an input
-  lcd.setCursor(0, 0);
-  lcd.print("Enter your phone:");
-  // Read bottle count and phone number from EEPROM on startup
-  readCountFromEEPROM();
+  struct PhoneData {
+    String phoneNumber;
+    int count;
+  };
+
+  PhoneData phoneData[MAX_PHONES];
+  int phoneDataSize = 0;
+
+  int findPhoneNumberIndex(String number) {
+    for (int i = 0; i < phoneDataSize; ++i) {
+      if (phoneData[i].phoneNumber == number) {
+        return i;
+      }
+    }
+    return -1;
+  }
+
+  void updatePhoneData(String number, int count) {
+  int index = findPhoneNumberIndex(number);
+
+  if (index != -1) {
+    // Phone number found, update the count if changed
+    phoneData[index].count += count;
+    int address = index * sizeof(PhoneData);
+    EEPROM.put(address, phoneData[index]);
+  } else {
+    if (phoneDataSize < MAX_PHONES) {
+      // Phone number not found, add it to EEPROM
+      phoneData[phoneDataSize].phoneNumber = number;
+      phoneData[phoneDataSize].count = count;
+      int address = phoneDataSize * sizeof(PhoneData);
+      EEPROM.put(address, phoneData[phoneDataSize]);
+      phoneDataSize++;
+    } else {
+      Serial.println("Maximum phone numbers reached!");
+    }
+  }
 }
 
-void loop() {
-  char key = keypad.getKey();
 
-  if (key != NO_KEY) {
-    if (key == '*') {
-      // Stop the 'B' key loop if '*' key is pressed
-      isBKeyPressed = false;
-      lcd.clear();
-      lcd.print("Enter your phone:");
-      lcd.setCursor(0, 1);
-      phoneNumber = "";
-      Serial.println("Clear Display");
-    } if (key == 'B' && !isBKeyPressed) {
-      // Start 'B' key loop if 'B' is pressed and loop is not already running
-      isBKeyPressed = true;
-      Serial.println("B key pressed");
-      checkSensors(); // Call checkSensors() immediately upon 'B' key press
-    }
+  void setup() {
+    Serial.begin(9600);
+    lcd.init();
+    lcd.backlight();
+    pinMode(trigPin, OUTPUT);
+    pinMode(echoPin, INPUT);
+    lcd.setCursor(0, 0);
+    lcd.print("Enter your phone:");
+    readCountFromEEPROM();
+  }
 
-    else if (key != '*' && key != '#' && key != 'B') {
-      // Handle keypad input except for '*', '#', and 'B'
-      if (isBKeyPressed) {
-        // If 'B' key loop is active
-        if (key == '#') {
-          isBKeyPressed = false; // Stop the loop when '#' is pressed
-          Serial.println("Loop stopped");
-        } else {
-          // Execute your desired loop functionality here
-          // For example:
-          // delay(1000);
-        }
+  void loop() {
+    char key = keypad.getKey();
+
+    if (key != NO_KEY) {
+      if (key == 'B') {
+        Serial.println("B key pressed");
+        lcd.clear();
+        lcd.setCursor(0, 0);
+        lcd.print("Bottle count: ");
+        lcd.print(bottleCount);
+        checkSensors();
+        continueCheckSensors = true;
+      } else if (key == 'C') {
+        checkPoints(); // Invoke checkPoints function
       } else {
-        // Handle regular keypad input
         if (isDigit(key)) {
           phoneNumber += key;
           lcd.setCursor(phoneNumber.length() - 1, 1);
@@ -88,129 +114,161 @@ void loop() {
             lcd.setCursor(phoneNumber.length(), 1);
             Serial.println("Deleted last digit");
           }
-        }  else if (key == 'A') {
-  if (phoneNumber.length() > 0) {
-    // Process the entered phone number
-    Serial.print("Entered phone number: ");
-    Serial.println(phoneNumber);
-    lcd.clear();
-    lcd.print("Complete");
-    delay(2000);
-    lcd.clear();
+        } else if (key == 'A') {
+          if (phoneNumber.length() > 0) {
+            Serial.print("Entered phone number: ");
+            Serial.println(phoneNumber);
 
-    // Save the entered phone number directly without concatenating
-    phoneNumber = ""; // Clear the phoneNumber before saving a new one
-    saveCountToEEPROM(bottleCount, phoneNumber);
-    
-    lcd.print("Enter your phone");
-    lcd.setCursor(0, 1);
-    phoneNumber = "";
-  }
-        } else if (key == 'C') {
-          // Display the count and phone number on LCD
-          displayInfoOnLCD();
+            // Save the entered phone number directly without concatenating
+            saveCountToEEPROM(1, phoneNumber);
+
+            lcd.clear();
+            lcd.print("Complete");
+            delay(2000);
+            lcd.clear();
+
+            lcd.print("Enter your phone");
+            lcd.setCursor(0, 1);
+            phoneNumber = "";
+          }
         }
       }
     }
   }
 
-  // Flag to indicate whether sensors should be checked
-  bool shouldRunCheckSensors = (key == 'B' && isBKeyPressed); // Only run checkSensors() when 'B' key is pressed and 'B' key loop is active
+  void checkSensors() {
+    while (true) {
 
-  if (shouldRunCheckSensors) {
-    checkSensors();
-  }
+      char key = keypad.getKey(); // Move this line inside the loop
 
-  
-}
+      unsigned long duration, distance;
 
-void checkSensors() {
-  for (int i = 0; i < 90; i++){
-  unsigned long duration, distance;
+      digitalWrite(trigPin, LOW);
+      delayMicroseconds(2);
+      digitalWrite(trigPin, HIGH);
+      delayMicroseconds(10);
+      digitalWrite(trigPin, LOW);
 
-  digitalWrite(trigPin, LOW);
-  delayMicroseconds(2);
-  digitalWrite(trigPin, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trigPin, LOW);
+      duration = pulseIn(echoPin, HIGH);
+      distance = duration * 0.034 / 2;
 
-  duration = pulseIn(echoPin, HIGH);
-  distance = duration * 0.034 / 2;
+      if (distance > 0 && distance < 50) {
+        bottleCount++;
+        Serial.print("Bottle count: ");
+        Serial.println(bottleCount);
+        Serial.println("run ran run");
+        lcd.clear();
+        lcd.setCursor(0, 0);
+        lcd.print("Bottle count: ");
+        lcd.print(bottleCount);
 
- 
+        delay(350);
 
-    if (distance > 0 && distance < 50) { // Assuming bottles are detected within 20 cm
-      bottleCount++;
-      Serial.print("Bottle count: ");
-      Serial.println(bottleCount);
-      Serial.println("run ran run");
-      lcd.clear();
-      lcd.setCursor(0, 0);
-      lcd.print("Bottle count: ");
-      lcd.print(bottleCount);
+          // Save bottle count to EEPROM with unique address based on the phone number
+          saveCountToEEPROM(bottleCount, phoneNumber);
+          updatePhoneData(phoneNumber, bottleCount);
+      }
 
-      delay(100);
-
-      // Save bottle count to EEPROM
-      saveCountToEEPROM(bottleCount, phoneNumber);
+        if (key != NO_KEY && key == '#') {
+        lcd.clear();
+        bottleCount = 0;
+        lcd.print("Enter your phone:");
+        lcd.setCursor(0, 1);
+        phoneNumber = "";
+        Serial.println("Clear Display");
+        break;  // Break out of the loop
+      }
     }
   }
-}
 
 
-void saveCountToEEPROM(int count, String number) {
-  int address = 0; // Start address in EEPROM
 
-  // Save bottle count
-  EEPROM.put(address, count);
-  address += sizeof(int); // Increment the address by the size of int
+  void saveCountToEEPROM(int count, String number) {
+    int phoneIndex = findPhoneNumberIndex(number);
 
-  // Save phone number
-  for (unsigned int i = 0; i < number.length(); ++i) {
-    EEPROM.put(address + i, number[i]);
-  }
-  EEPROM.put(address + number.length(), '\0'); // Null-terminate the phone number
-}
+    if (phoneIndex == -1) {
+      // Phone number not found, add it to EEPROM
+      if (phoneDataSize < MAX_PHONES) {
+        int address = phoneDataSize * sizeof(PhoneData);
+        EEPROM.put(address, PhoneData{number, count});
+        phoneDataSize++;
+      } else {
+        Serial.println("Maximum phone numbers reached!");
+      }
+    } else {
+      // Phone number found, update the count if changed
+      int address = phoneIndex * sizeof(PhoneData);
+      EEPROM.get(address, phoneData[phoneIndex]);
 
-void readCountFromEEPROM() {
-  int address = 0; // Start address in EEPROM
-
-  // Read bottle count
-  EEPROM.get(address, bottleCount);
-  address += sizeof(int); // Increment the address by the size of int
-
-  // Read phone number
-  String tempPhone = "";
-  char ch;
-  do {
-    EEPROM.get(address + tempPhone.length(), ch);
-    if (ch != '\0') {
-      tempPhone += ch;
+      if (phoneData[phoneIndex].count != count) {
+        phoneData[phoneIndex].count = count;
+        EEPROM.put(address, phoneData[phoneIndex]);
+      }
     }
-  } while (ch != '\0');
-  phoneNumber = tempPhone;
+  }
+  void readCountFromEEPROM() {
+    for (int i = 0; i < phoneDataSize; ++i) {
+      int address = i * sizeof(PhoneData);
+      EEPROM.get(address, phoneData[i]);
+    }
 
-  Serial.print("Stored Count: ");
-  Serial.println(bottleCount);
-  Serial.print("Stored Phone Number: ");
-  Serial.println(phoneNumber);
-}
+    Serial.println("Stored Data:");
+    for (int i = 0; i < phoneDataSize; ++i) {
+      Serial.print("Phone Number: ");
+      Serial.print(phoneData[i].phoneNumber);
+      Serial.print(", Count: ");
+      Serial.println(phoneData[i].count);
+    }
+  }
 
+  void displayInfoOnLCD() {
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("Phone: ");
+    lcd.print(phoneNumber);
 
-void displayInfoOnLCD() {
-  lcd.clear();
-  lcd.setCursor(0, 0);
-  lcd.print("Count: ");
-  lcd.print(bottleCount);
-  Serial.print("Count: ");	
-  Serial.print(bottleCount);
-  
-   
-  lcd.setCursor(0, 1);
-  lcd.print("Phone: ");
-  lcd.print(phoneNumber);
-  delay(7500);
-  lcd.clear();
-  lcd.print("Enter your phone");
+    lcd.setCursor(0, 1);
+    lcd.print("Total Count: ");
+    int total = 0;
+    for (int i = 0; i < phoneDataSize; ++i) {
+      if (phoneData[i].phoneNumber == phoneNumber) {
+        total = phoneData[i].count;
+        break;
+      }
+    }
+    lcd.print(total);
 
-}
+    Serial.print("Phone: ");
+    Serial.print(phoneNumber);
+    Serial.print(", Total Count: ");
+    Serial.println(total);
+
+    delay(7500);
+    lcd.clear();
+    lcd.print("Enter your phone");
+  }
+
+  void checkPoints() {
+    int index = findPhoneNumberIndex(phoneNumber);
+
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("Phone: ");
+    lcd.print(phoneNumber);
+
+    lcd.setCursor(0, 1);  
+    lcd.print("Total Count: ");
+    int total = (index != -1) ? phoneData[index].count : 0;
+    lcd.print(total);
+
+    Serial.print("Phone: ");
+    Serial.print(phoneNumber);
+    Serial.print(", Total Count: ");
+    Serial.println(total);
+
+    delay(5000);
+    lcd.clear();
+    Serial.println("Clear Check");
+    phoneNumber = "";
+    lcd.print("Enter your phone");
+  }
